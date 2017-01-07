251286ca6df1388b9b4dc2375d9f34f2e0d01524
root <root@#localhost.localdomain>
2017-01-07 20:09:02 +0900

    add some lines for explain

--- linux_os/custom_cp.c
+++ linux_os/custom_cp.c
@@ -4,7 +4,6 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
-#include <errno.h>
 
 void copyFile(char *input, char *output)
 {
@@ -13,9 +12,13 @@ void copyFile(char *input, char *output)
 	
 	char buff[1024]; //buffer
 
+	//open the input file
 	in_o = open(input, O_RDONLY);
-	out_o = open(output, O_WRONLY | O_CREAT, S_IWUSR);
+
+	//open the output file, when the output file not exist, create new one
+	out_o = open(output, O_WRONLY | O_CREAT, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
 	
+	//copy all lines by buffer
 	while((read_o = read(in_o, buff, sizeof(buff))) > 0){
 		write(out_o, buff, read_o);
 	}
@@ -23,5 +26,11 @@ void copyFile(char *input, char *output)
 
 int main(int argc, char **argv)
 {
-	copyFile(argv[1], argv[2]);
+	//if input source not exist
+	if(access(argv[1], F_OK) != 0){
+		copyFile(argv[1], argv[2]);
+	}else {
+		//print error message
+		printf("the input is not exist\n");
+	}
 }
